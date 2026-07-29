a1217a850e8e2a22a3218f85812a778a53266e2a
elarif <elarif@local>
2026-07-29 23:46:57 +0200

    chore(web): add React islands support for V1

--- web/astro.config.ts
+++ web/astro.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import react from "@astrojs/react";
 
 export default defineConfig({
   site: process.env.SITE_URL || "https://litteratureaudio.pages.dev",
   output: "static",
   trailingSlash: "never",
   build: { format: "file" },
-  integrations: [tailwind({ applyBaseStyles: false }), sitemap()],
+  integrations: [tailwind({ applyBaseStyles: false }), sitemap(), react()],
 });
